c43aed04e983775db8891779105f8ef9da1ceee5
kirti <kirtiadikane22@gmail.com>
2023-10-18 14:16:28 +0530

    JPA repository

--- currency-exchange-service/src/main/java/com/kirti/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/kirti/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,15 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
 	@Autowired
+	private CurrencyExchangeRepository currencyExchangeRepository;
+	
+	@Autowired
 	private Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
 			@PathVariable String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000l, from, to, BigDecimal.valueOf(50));
+		//CurrencyExchange currencyExchange = new CurrencyExchange(1000l, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+		
+		if(currencyExchange == null)
+			throw new RuntimeException("Unable to find data for " + from + " to " + to );
+		
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;		
 	}
+	
 	
 }
