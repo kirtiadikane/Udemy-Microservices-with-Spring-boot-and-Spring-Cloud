97524e299c665ddd5a371382011e0fc91c6cdd92
kirti <kirtiadikane22@gmail.com>
2023-10-18 15:53:21 +0530

    invoke currency-exchange from currency-conversion microservice

--- currency-conversion-service/src/main/java/com/kirti/microservice/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/kirti/microservice/currencyconversionservice/CurrencyConversion.java
@@ -7,8 +7,8 @@ public class CurrencyConversion {
 	private Long id;
 	private String from;
 	private String to;
-	private BigDecimal conversionMultiple;
 	private BigDecimal quantity;
+	private BigDecimal conversionMultiple;
 	private BigDecimal totalCalculatedAmount;
 	private String environment;
 	
@@ -16,7 +16,7 @@ public class CurrencyConversion {
 		
 	}
 
-	public CurrencyConversion(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+	public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, 
 			BigDecimal totalCalculatedAmount, String environment) {
 		super();
 		this.id = id;
