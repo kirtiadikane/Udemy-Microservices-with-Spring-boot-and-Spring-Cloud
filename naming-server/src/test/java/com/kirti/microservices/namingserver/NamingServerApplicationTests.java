e37e612f93ba7acc4a2fdaffbc62b048c8905420
kirti <kirtiadikane22@gmail.com>
2023-10-18 17:14:29 +0530

    eureka naming server set up

---
+++ naming-server/src/test/java/com/kirti/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirti.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
