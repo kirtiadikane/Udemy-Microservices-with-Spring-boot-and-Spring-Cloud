dde8f3312cd6609a0f3207119837fe30f9dd768c
kirti <kirtiadikane22@gmail.com>
2023-10-20 15:07:02 +0530

    Resilience4j Bulkhead feature

--- currency-exchange-service/src/main/java/com/kirti/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/kirti/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -19,7 +20,8 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api-retry-kirti", fallbackMethod = "hardcodedResponse")
 	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-	@RateLimiter(name="default-rate-limiter-kirti")
+	//@RateLimiter(name="default-rate-limiter-kirti")
+	@Bulkhead(name = "default-bulkhead-kirti")
 	public String sampleApi() {
 		logger.info("Sample API call received");
 //		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
